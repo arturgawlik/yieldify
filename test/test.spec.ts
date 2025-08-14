a2c93734923c3cd32b8d29144b40a6b23d3fcca7
arturgawlik <artur.gawlik@icloud.com>
2025-08-14 18:05:49 +0200

    handle multiple `yieldifiedEnv`s through `AsyncLocalStorage`

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,16 +1,42 @@
 import t from "tap";
-import { readFile } from "node:fs";
+import { readFile, readdir } from "node:fs";
 import { yieldifiedEnv, yieldify } from "../yieldify.ts";
 
-t.test("yieldify", (t) => {
+const testFileOnePath = import.meta
+  .resolve("./fixtures/test-file-one.txt")
+  .replace("file://", "");
+const testFileTwoPath = import.meta
+  .resolve("./fixtures/test-file-two.txt")
+  .replace("file://", "");
+
+t.test("should allow to yield 'node:fs'.readFile", (t) => {
   yieldifiedEnv(function* () {
     const readFileYieldified = yieldify(readFile);
     t.ok(
       typeof readFileYieldified === "function",
       "should create yieldified version of a function"
     );
-    const file = yield readFileYieldified(import.meta.filename, "utf8");
-    t.ok(/tap/.test(file), "should read file with yieldified function");
+    const file = yield readFileYieldified(testFileOnePath, "utf8");
+    t.ok(/test-file-one/.test(file), `wrong file content: "${file}"`);
     t.end();
+  });
+});
+
+t.test("should allow multiple 'yieldifiedEnv's at the same time", (t) => {
+  let endCalls = 0;
+  const end = () => {
+    if (++endCalls === 2) t.end();
+  };
+  yieldifiedEnv(function* () {
+    const readFileYieldified = yieldify(readFile);
+    const file = yield readFileYieldified(testFileOnePath, "utf8");
+    t.ok(/test-file-one/.test(file), `wrong file content: "${file}"`);
+    end();
+  });
+  yieldifiedEnv(function* () {
+    const readFileYieldified = yieldify(readFile);
+    const file = yield readFileYieldified(testFileTwoPath, "utf8");
+    t.ok(/test-file-two/.test(file), `wrong file content: "${file}"`);
+    end();
   });
 });
--- yieldify.ts
+++ yieldify.ts
@@ -1,12 +1,16 @@
-let iterator: Iterator<unknown>;
+import { AsyncLocalStorage } from "node:async_hooks";
+const iteratorsStorage = new AsyncLocalStorage<Iterator<unknown>>();
 export function yieldifiedEnv(topLevelGenerator: () => Generator) {
-  iterator = topLevelGenerator();
-  iterator.next();
+  const iterator = topLevelGenerator();
+  iteratorsStorage.run(iterator, () => {
+    iterator.next();
+  });
 }
 // TODO: improve TS of this
 export function yieldify(fn: (...args: any) => any) {
   return (...args: any): any => {
     fn(...args, (err: unknown, res: unknown): any => {
+      const iterator = iteratorsStorage.getStore();
       if (assertIterator(iterator)) {
         // @ts-ignore FIXME
         if (err) iterator.throw(err);
